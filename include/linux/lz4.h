ce71996eafca5dc2fd0677d1b62c60137f9c919f
Panchajanya1999 <panchajanya@azure-dev.live>
2021-11-22 09:36:06 +0530

    include/linux: lz4: Increase LZ4 memory usage to 64KB
    64KB perfectly fits into Kryo 460 Silver L1 cache. Improves
    compression ratio too.
    
    Change-Id: I579a91c7bdb4ee4ac73bd86871b7f6a683b856f8
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- include/linux/lz4.h
+++ include/linux/lz4.h
@@ -56,7 +56,6 @@
  * Default value is 14, for 16KB, which nicely fits into Intel x86 L1 cache
  */
 #define LZ4_MEMORY_USAGE 16
-
 #define LZ4_MAX_INPUT_SIZE	0x7E000000 /* 2 113 929 216 bytes */
 #define LZ4_COMPRESSBOUND(isize)	(\
 	(unsigned int)(isize) > (unsigned int)LZ4_MAX_INPUT_SIZE \
