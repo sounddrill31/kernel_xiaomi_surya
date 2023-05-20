a41208c3d6e56ab3a5be72a147145e948c611f8c
Richard Raya <rdxzv.dev@gmail.com>
2023-05-20 00:16:46 -0300

    build.sh: Switch to Slim LLVM 16 toolchain
    Change-Id: If19e2e0403c326eb145f69254447c073dcc3507e
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 
 SECONDS=0 # builtin bash timer
 ZIPNAME="QuicksilveR-surya-$(date '+%Y%m%d-%H%M').zip"
-TC_DIR="$(pwd)/tc/clang-r487747"
+TC_DIR="$(pwd)/tc/slim-16"
 AK3_DIR="$(pwd)/android/AnyKernel3"
 DEFCONFIG="vendor/surya-perf_defconfig"
 
@@ -17,8 +17,8 @@ fi
 export PATH="$TC_DIR/bin:$PATH"
 
 if ! [ -d "$TC_DIR" ]; then
-	echo "AOSP clang not found! Cloning to $TC_DIR..."
-	if ! git clone --depth=1 -b 17 https://gitlab.com/ThankYouMario/android_prebuilts_clang-standalone "$TC_DIR"; then
+	echo "Slim LLVM not found! Cloning to $TC_DIR..."
+	if ! git clone --depth=1 -b slim-16 https://gitlab.com/ThankYouMario/android_prebuilts_clang-standalone "$TC_DIR"; then
 		echo "Cloning failed! Aborting..."
 		exit 1
 	fi
